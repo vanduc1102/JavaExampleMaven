823926ddb844e2182792cb015de0ca248e71d3c1
Duc Nguyen <vanducld@gmail.com>
2016-05-28 08:46:39 +0700

    Correc the root folder

--- src/main/java/my/example/aspose/ConvertHTMLtoPDF.java
+++ src/main/java/my/example/aspose/ConvertHTMLtoPDF.java
@@ -1,5 +1,8 @@
 package my.example.aspose;
 
+import com.aspose.pdf.Document;
+import com.aspose.pdf.HtmlLoadOptions;
+import com.aspose.pdf.License;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -15,13 +18,16 @@ public class ConvertHTMLtoPDF {
         System.out.println("Current relative path is: " + s);
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
         checkCurrentPath();
 //        String basePath = "./src/main/resources/email-hule.html";
 //        String basePath = "./src/main/resources/email-inlined.html";
-        String basePath = "./src/main/resources/simple.html";
-        com.aspose.pdf.HtmlLoadOptions htmloptions = new com.aspose.pdf.HtmlLoadOptions(basePath);
-        com.aspose.pdf.Document doc = new com.aspose.pdf.Document(basePath, htmloptions);
-        doc.save("./target/hele.pdf");
+        //String basePath = ""./src/main/resources/simple.html";
+        License license = new License();
+        license.setLicense(".\\src\\main\\resources\\Aspose.Pdf.lic");
+        String basePath = "./src/main/resources/email-inlined.html";
+        HtmlLoadOptions htmloptions = new HtmlLoadOptions(basePath);
+        Document doc = new Document(basePath, htmloptions);
+        doc.save(".\\target\\hele.pdf");
     }
 }
