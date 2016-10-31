37d43e75717981dbcf38124f2d5f3c72ccc9130b
Duc NGUYEN <vanducld@gmail.com>
2016-10-31 16:14:47 +0700

    Add write huge string to file

---
+++ src/main/java/my/example/file/system/WriteHugeString.java
@@ -0,0 +1,19 @@
+package my.example.file.system;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ *
+ * @author nvduc
+ */
+public class WriteHugeString {
+    public static void main(String[] args) throws IOException {
+        String hugeString = "https://www.google.com.vn/search?q=ejb+get+data+from+future+object&oq=ejb+get+data+from+future+object&aqs=chrome..69i57.9047j0j4&sourceid=chrome&ie=UTF-8#q=ejb+get+data+from+future+object+example";
+        Path temp = Files.createTempFile("aaaaaa", "hugeString");
+        Files.write(temp, hugeString.getBytes());
+        System.out.println("Path : "+temp.toFile().getAbsolutePath());
+    }
+    
+}
